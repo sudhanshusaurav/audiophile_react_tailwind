ccc1fc1339969e924194c860160ed2b005ea490f
sudhanshu_saurav <sudhanshussaurav@gmail.com>
2021-12-13 01:06:49 +0530

    home links add

--- src/components/NewArrival.jsx
+++ src/components/NewArrival.jsx
@@ -38,13 +38,13 @@ function NewArrival() {
                 <div className='text-center text-white flex-1'>
                     <h1>ZX9 <br /> SPEAKER</h1>
                     <p className='tracking-wide px-4 my-6 md:mb-8 md:w-1/2 mx-auto'>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
-                    <Link to="" className='py-5 px-10 inline-block tracking-wider bg-secondary hover:bg-gray-700'>SEE PRODUCT</Link>
+                    <Link to="/product/zx9-speaker" className='py-5 px-10 inline-block tracking-wider bg-secondary hover:bg-gray-700'>SEE PRODUCT</Link>
                 </div>
                 <img src={BgPattern} alt="circles" className='absolute lg:w-1/2 top-0 -z-1'/>
             </div>
             <div className='rounded-lg bg-zx-7 md:bg-zx-7-md lg:bg-zx-7-lg bg-cover px-4 py-20 md:px-12 my-8'>
                 <h3 className='mb-8 mt-4'>ZX7 SPEAKER</h3>
-                <Link to="" className='inline-block py-5 px-10 mb-4 border border-secondary hover:bg-secondary hover:text-white'>SEE PRTODUCT</Link>
+                <Link to="/product/zx7-speaker" className='inline-block py-5 px-10 mb-4 border border-secondary hover:bg-secondary hover:text-white'>SEE PRTODUCT</Link>
             </div>
 
             <div className='md:flex gap-4 items-stretch'>
@@ -53,7 +53,7 @@ function NewArrival() {
                 </div>
                 <div className='flex-1 rounded-lg bg-greyLight py-8 md:py-20 lg:py-24 px-4 lg:px-12 my-8 md:my-0'>
                     <h3 className='mb-8'>YX1 EARPHONES</h3>
-                    <Link to="" className='inline-block py-5 px-10 mb-4 border border-secondary hover:bg-secondary hover:text-white'>SEE PRODUCT</Link>
+                    <Link to="/product/yx1-earphones" className='inline-block py-5 px-10 mb-4 border border-secondary hover:bg-secondary hover:text-white'>SEE PRODUCT</Link>
                 </div>
             </div>
         </div>
